fdb3ba71f09ba78c6bbc76b61ee012e6166e3792
jriccrussel <jriccrussel@gmail.com>
2021-11-09 19:38:46 +0800

    complete hero | typewritter-effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 const Home = () => {
     return (
-        <div>
-            <h1>HOMEltI</h1>
-        </div>
+        <>
+            <div className="hero">
+                <div className="header">
+                    {/* <h1 className="line">I make brands matter in culture.</h1> */}
+                    <Typewriter 
+                        onInit={(type) => {
+                            type.typeString("I make brands matter in culture.")
+                            .pauseFor(4000)
+                            .deleteAll()
+                            .typeString("I create to inspire.")
+                            .start()
+                        }}
+
+                        options={{
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </div>
+            </div>
+
+            <div className="scroll-down"></div>
+        </>
     )
 }
 
