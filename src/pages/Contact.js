bc764eeeb3a2dbe0599a9e7c03880bd6123fe9e6
jriccrussel <jriccrussel@gmail.com>
2021-11-10 16:41:20 +0800

    complete contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,65 @@
 import React from 'react'
+import {WOW} from 'wowjs'
 
 const Contact = () => {
     return (
-        <div>
-            COntact
-        </div>
+        <>
+            <div className="whitespace"></div>
+
+            <div className="abt_content">
+                <div className="wrapper">
+                    <br/><br/>
+                    <div className="work_wrapper flex">
+                        <div className="w_left">
+                            <h3 class="wow fadeInUp" data-wow-delay="1s">say hello 👋</h3><br/>
+                            <p class="wow fadeInUp" data-wow-delay="1.2s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, accusantium sint quod voluptatem dolorum, quaerat, ut iusto cumque magni ab repudiandae, quasi minus unde temporibus nulla eum eaque! Placeat praesentium sunt suscipit laboriosam deserunt commodi atque corporis, iure maiores dicta, fugit similique.</p>
+                        </div>
+
+                        <div className="w_right"></div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className="sect_form">
+                <div className="wrapper flex">
+                    <div className="w_left">
+                        <form action="" name="contact-form" id="contact-form">
+                            <div className="input-row wow fadeInUp" data-wow-delay="1.4s">
+                                <label for="contact-name">Name :</label>
+                                <div class="textarea">
+                                    <input type="text" name="contact-name" id="contact-name" value="" required />
+                                </div>
+                            </div>
+
+                            <div className="input-row wow fadeInUp" data-wow-delay="1.6s">
+                                <label for="contact-email">Email :</label>
+                                <div class="textarea">
+                                    <input type="email" name="contact-email" id="contact-email" value="" required/>
+                                </div>
+                            </div>
+
+                            <div className="input-row wow fadeInUp" data-wow-delay="1.6s">
+                                <label for="contact-project">Message :</label>
+                                <div class="textarea">
+                                    <textarea type="email" name="contact-project" id="contact-project" rows="6" value="" required>
+                                    </textarea>
+                                </div>
+                            </div>
+
+                            <br/><br/>
+
+                            <button id="contact-submit" className="send wow fadeInUp" type="submit" name="contact-submit">Send Message</button>                            
+
+                        </form>
+                    </div>
+
+                    <div className="w_right"></div>
+                </div>
+            </div>
+
+            <div className="whitespace"></div>
+        </>
     )
 }
 
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,10 +1,87 @@
 import React from 'react'
+import {WOW} from 'wowjs'
 
 const Work = () => {
     return (
-        <div>
-            Work
-        </div>
+        <>  
+            <div className="whitespace"></div>
+
+            <div className="abt_content">
+                <div className="wrapper">
+                    <br/><br/>
+                    <div className="work_wrapper flex">
+                        <div className="w_left">
+                            <h3 className="wow fadeInUp" data-wow-delay="1s">work.</h3>
+                            <p class="wow fadeInUp" data-wow-delay="1.2s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, accusantium sint quod voluptatem dolorum, quaerat, ut iusto cumque magni ab repudiandae, quasi minus unde temporibus nulla eum eaque! Placeat praesentium sunt suscipit laboriosam deserunt commodi atque corporis, iure maiores dicta, fugit similique.</p>
+                        </div>
+
+                        <div className="w_right"></div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className="sect_project">
+
+                <div className="whitespace"></div>
+                <div className="whitespace"></div>
+
+                <div className="flex project1">
+                    <div className="w_left"></div>
+
+                    <div className="w_right">
+                        <div className="prj1 wow fadeInUp" data-wow-delay="0.4s">
+                            <img className="wow fadeInUp" data-wow-delay="0.2s" src="https://images.unsplash.com/photo-1540349220179-09d3866f2e74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80" alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="whitespace"></div>
+
+                <div className="flex project2">
+                    <div className="w_left">
+                        <div className="prj2 wow fadeInUp" data-wow-delay="0.4s">
+                            <img className="wow fadeInUp" data-wow-delay="0.2s" src="https://images.unsplash.com/photo-1468870045442-e7d799d8c98a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2448&q=80" alt="" />
+                        </div>
+                    </div>
+
+                    <div className="w_right"></div>
+                </div>
+
+                <div className="whitespace"></div>
+
+                <div className="flex project3">
+                    <div className="w_left">
+
+                    </div>
+
+                    <div className="w_center">
+                        <div className="prj3 wow fadeInUp" data-wow-delay="0.4s">
+                            <img className="wow fadeInUp" data-wow-delay="0.2s" src="https://images.unsplash.com/photo-1482501157762-56897a411e05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" alt="" />
+                        </div>
+                    </div>
+
+                    <div className="w_right"></div>
+                </div>
+
+                <div className="whitespace"></div>
+
+                <div className="flex project4">
+                    <div className="w_left"></div>
+
+                    <div className="w_center">
+                        <div className="prj4 wow fadeInUp" data-wow-delay="0.4s">
+                            <img className="wow fadeInUp" data-wow-delay="0.2s" src="https://images.unsplash.com/photo-1435685813800-51ba4ceb9c4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1785&q=80" alt="" />
+                        </div>
+                    </div>
+
+                    <div className="w_right"></div>
+                </div>
+
+                <div className="whitespace"></div>
+
+            </div>
+        </>
     )
 }
 
