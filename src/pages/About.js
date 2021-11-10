11e337efadc54e343ad7c8c24634243b94c4b6d8
jriccrussel <jriccrussel@gmail.com>
2021-11-10 09:40:54 +0800

    complete about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import {WOW} from 'wowjs'
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <>
+            <div className="sect_abt_hero">
+                <div className="wrapper">
+                    <img className="wow fadeInUp" data-wow-delay="0s" src="https://images.unsplash.com/photo-1540885762261-a2ca01f290f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2468&q=80" alt="" />
+                </div>
+            </div>
+
+            <div className="abt_content">
+                <div className="wrapper">
+                    <br/><br/>
+
+                    <h3 className="wow fadeInUp" data-wow-delay="0.2s">about me.</h3>
+
+                    <p class="wow fadeInUp" data-wow-delay="0.4s">Living with passion is what I strictly adhere to. “Always do what you love” how much ever hackneyed this sounds it holds absolutely true. That's why I travel a lot. Not for the sole reason of “loving” to travel but it helps me to get rid of the world and escape into a different dimension of my own. As I am a travel freak I love freezing the moments I relish. The art of capturing photos is something I am always fascinated by.</p>
+
+                    <p class="wow fadeInUp" data-wow-delay="0.6s">I am a continuous learner. Learning for me equates to exploring new ideas and growing as an individual. I just cannot imagine my life devoid of designing. I freely express my creativity and imagination by designing. And to give my passion of designing a platform I have a YouTube channel named, Codegrid.</p>
+
+                </div>
+            </div>
+        </>
     )
 }
 
